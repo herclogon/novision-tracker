2f6682bac0eb32bf6a907ceca5274555c49c4589
Dmitry Golubkov <master@hsdesign.ru>
2020-10-29 00:23:44 +0300

    tmp

--- tracker.js
+++ tracker.js
@@ -175,8 +175,26 @@ function speechDemo() {
     console.log({ voices });
     var utterThis = new SpeechSynthesisUtterance("привет всем, как дела?");
     utterThis.lang = "ru-RU";
-    utterThis.voice = voices[0];
+    // utterThis.voice = voices[12];
     synth.speak(utterThis);
     // inputTxt.blur();
+}
+function textRecognition() {
+    var grammar = "#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;";
+    var recognition = new webkitSpeechRecognition();
+    var speechRecognitionList = new webkitSpeechGrammarList();
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = speechRecognitionList;
+    recognition.continuous = false;
+    recognition.lang = "ru-RU";
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+    recognition.start();
+    recognition.onresult = function (event) {
+        console.log(event.results);
+        // var color = event.results[0][0].transcript;
+        // diagnostic.textContent = "Result received: " + color;
+        // bg.style.backgroundColor = color;
+    };
 }
 //# sourceMappingURL=tracker.js.map
--- tracker.ts
+++ tracker.ts
@@ -208,7 +208,28 @@ function speechDemo() {
 
   var utterThis = new SpeechSynthesisUtterance("привет всем, как дела?");
   utterThis.lang = "ru-RU";
-  utterThis.voice = voices[0];
+  // utterThis.voice = voices[12];
   synth.speak(utterThis);
   // inputTxt.blur();
+}
+
+function textRecognition() {
+  var grammar =
+    "#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;";
+  var recognition = new SpeechRecognition();
+  var speechRecognitionList = new SpeechGrammarList();
+  speechRecognitionList.addFromString(grammar, 1);
+  recognition.grammars = speechRecognitionList;
+  recognition.continuous = false;
+  recognition.lang = "ru-RU";
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+  recognition.start();
+
+  recognition.onresult = function (event) {
+    console.log(event.results);
+    // var color = event.results[0][0].transcript;
+    // diagnostic.textContent = "Result received: " + color;
+    // bg.style.backgroundColor = color;
+  };
 }
