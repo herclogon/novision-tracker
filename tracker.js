ce7b09f57b7876aeb7c668fd8ba4b31adc3cfee9
Dmitry Golubkov <master@hsdesign.ru>
2020-10-28 22:59:21 +0300

    tmp

--- tracker.js
+++ tracker.js
@@ -1,5 +1,17 @@
 var a = function () {
     console.log("TEST !");
+    var h = 1;
 };
+function showPosition(position) {
+    console.log(position.coords, "Latitude: " +
+        position.coords.latitude +
+        "<br>Longitude: " +
+        position.coords.longitude);
+}
+if (navigator.geolocation) {
+    setInterval(function () {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }, 1000);
+}
 // export { a };
 //# sourceMappingURL=tracker.js.map
--- tracker.ts
+++ tracker.ts
@@ -1,5 +1,22 @@
 let a = () => {
   console.log("TEST !");
+  var h = 1;
 };
+
+function showPosition(position) {
+  console.log(
+    position.coords,
+    "Latitude: " +
+      position.coords.latitude +
+      "<br>Longitude: " +
+      position.coords.longitude
+  );
+}
+
+if (navigator.geolocation) {
+  setInterval(() => {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }, 1000);
+}
 
 // export { a };
